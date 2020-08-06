a6f758a452a6216c1c78e6f1a8f80dc20cb267d2
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-07 02:48:02 +0300

    All components are temporarily removed from the file

---
+++ src/Components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormMap from './FormMap/FormMap';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <FormMap />
+        </div>
+    );
+}
+
+export default App;
